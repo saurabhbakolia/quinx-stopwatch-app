dd4eb00528b70c18ec5ac5752cdd4848b2f717eb
Saurabh Bakolia <78996216+saurabhbakolia@users.noreply.github.com>
2023-12-15 06:26:35 +0530

    response added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
-import Image from 'next/image'
+'use client'
+import Link from 'next/link';
+import { useRouter } from 'next/navigation'
+import useMemo from 'next/router';
+import { useEffect } from 'react';
 
 export default function Home() {
+  const router = useRouter();
+
   return (
     <main>
-      <h1>Stop Watch!</h1>
+      <Link href='/stop-watch'>
+        Click Here to Use the Stopwatch 🙂
+      </Link>
     </main>
   )
 }
--- app/stop-watch/page.tsx
+++ app/stop-watch/page.tsx
@@ -45,11 +45,11 @@ function StopWatch() {
 
     return (
         <main className='grid grid-cols-1 place-items-center bg-gray-100 h-screen m-auto'>
-            <div className='bg-gray-300 w-[100%] h-[100%] md:w-[90%] md:h-[70%] lg:w-[70%] lg:h-[60%] xl:w-[50%] 2xl:w-[40%] 2xl:h-[64%] md:px-10 md:py-6 grid grid-cols-1 lg:grid-cols-3 grid-rows-4 gap-8'>
+            <div className='bg-gray-300 w-[100%] h-[100%] md:w-[90%] md:h-[70%] lg:w-[70%] lg:h-[60%] xl:w-[50%] 2xl:w-[40%] 2xl:h-[64%] md:px-10 md:py-6 grid grid-cols-1 lg:grid-cols-3 grid-rows-6 lg:grid-rows-4 lg:gap-8 grid-flow-col lg:grid-flow-row'>
                 <div className='bg-slate-900 col-span-3 row-span-1'>
                     <h1 className='text-blue-400 text-[4rem] text-center d-block align-middle'>{formatTime(time)}</h1>
                 </div>
-                <div className='row-span-4  text-white flex flex-col gap-6 '>
+                <div className='row-span1 lg:row-span-4  text-white flex flex-col gap-1 lg:gap-6 w-[100%]'>
                     <button
                         className='text-[2em] bg-sky-400 w-[100%] basis-1/3 drop-shadow-xl'
                         onClick={startStopwatch}
@@ -72,7 +72,7 @@ function StopWatch() {
                         Reset
                     </button>
                 </div>
-                <div className='col-span-2 p-4 bg-blue-400 row-span-4 max-h-96 overflow-scroll'>
+                <div className='col-span-2 p-4 row-span-4 max-h-96 overflow-scroll my-10 lg:my-0'>
                     <table className="table-auto w-full text-center text-xl text-gray-500">
                         <tbody>
                             {timeStampHistory.map((timeStamp, index) => (
